900cd73e62d3be1fc3fddb491b3e65a62b082a20
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-04-29 00:55:56 +0800

    Update SendEmailWithAttachment.py

--- SendEmailWithAttachment.py
+++ SendEmailWithAttachment.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3 
 
 from email.message import EmailMessage
-from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.platypus import Paragraph, Spacer, Table,Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import SimpleDocTemplate
 
